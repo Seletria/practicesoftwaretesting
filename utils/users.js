085363433d24197d239dbcf7e1c70c6deff0a5ca
Seletria <esra.kocaoglu.contact@gmail.com>
2026-06-08 22:55:12 +0300

    add util users

---
+++ utils/users.js
@@ -0,0 +1,19 @@
+export const users = {
+  admin: {
+    email: "admin@practicesoftwaretesting.com",
+    password: "welcome01"
+  },
+  user1: {
+    email: "customer@practicesoftwaretesting.com",
+    password: "welcome01"
+  },
+  user2: {
+    email: "customer2@practicesoftwaretesting.com",
+    password: "welcome01"
+  },
+  user3: {
+    email: "customer3@practicesoftwaretesting.com",
+    password: "pass123"
+  }
+
+}
