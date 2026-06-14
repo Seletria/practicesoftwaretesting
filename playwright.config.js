f6c5b2f480bcb4d9c989dc139fd6c464401862f5
Seletria <esra.kocaoglu.contact@gmail.com>
2026-06-14 15:50:25 +0300

    added userAgent

--- playwright.config.js
+++ playwright.config.js
@@ -33,6 +33,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+
   },
 
   /* Configure projects for major browsers */
